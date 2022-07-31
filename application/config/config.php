384c9235b983f70dd2db19f4e8959f5c5e449219
Firman <moh.firmansyah@student.ub.ac.id>
2022-07-31 22:35:30 +0700

    master

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'https://karangnongko-poncokusumo.com/';
-$config['base_url'] = 'localhost/betatest/';
+$config['base_url'] = 'http://localhost/betatest/';
 
 /*
 |--------------------------------------------------------------------------
