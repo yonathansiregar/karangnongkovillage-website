1856c44366888e1b47d28461966f7ba73a5f2f85
Firman <moh.firmansyah@student.ub.ac.id>
2022-07-31 22:53:50 +0700

    master

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://karangnongko-poncokusumo.com/';
-// $config['base_url'] = 'http://localhost/betatest/';
+// $config['base_url'] = 'https://karangnongko-poncokusumo.com/';
+$config['base_url'] = 'http://localhost/betatest/';
 
 /*
 |--------------------------------------------------------------------------
