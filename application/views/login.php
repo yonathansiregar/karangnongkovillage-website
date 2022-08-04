dda0dca4e04ce08218c3657b3e4bd924cf4b7c79
cnovandinata <95232744+cnovandinata@users.noreply.github.com>
2022-08-04 22:55:23 +0700

    login

--- application/views/login.php
+++ application/views/login.php
@@ -2,28 +2,11 @@
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
   <!-- Icon -->
-  <link rel="icon" href="assets/images/Logo.png" type="images/png">
-
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-  <!-- Slick CSS -->
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css">
-
-  <!-- Homepage CSS -->
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/global.css">
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/homepage.css">
-
-  <!-- TITLE PAGE -->
-  <title><?php echo $title; ?></title><!-- Icon -->
-  <link rel="icon" href="assets/images/Logo.png" type="images/png">
+  <link rel="icon" href="<?php echo base_url(); ?>assets/images/Logo.png" type="images/png">
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -35,6 +18,7 @@
   <!-- Homepage CSS -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/global.css">
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/login.css">
+
 
   <!-- TITLE PAGE -->
   <title><?php echo $title; ?></title>
@@ -137,6 +121,31 @@
     </section>
     <section class="page-title container mt-5">
       <p>LOGIN</p>
+    </section>
+    <section class="my-5 products-list d-flex justify-content-center align-items-center">
+      <div class="card horizontal-card mb-3 p-5" style="max-width: 1100px;">
+        <form action="">
+          <div class="row g-3 align-items-center" style="margin-bottom:40px;">
+            <div class="col-3">
+                <label for="username" class="form-label">USERNAME</label>
+            </div>
+            <div  class="col-3">
+                <input type="text" class="form-field" id="username">
+            </div>
+          </div>
+          <div class="row g-3 align-items-center"  style="margin-bottom:60px;">
+            <div class="col-3">
+                <label for="password" class="form-label">PASSWORD</label>
+            </div>
+            <div  class="col-3">
+                <input type="password" class="form-field" id="password">
+            </div>
+          </div>
+          <div style="margin-bottom:100px;">
+            <input type="button" value="LOGIN" class="form-btn">
+          </div>
+        </form>
+      </div>
     </section>
   </div>
   <!-- End of MAIN CONTENTS -->
