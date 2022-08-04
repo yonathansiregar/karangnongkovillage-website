38311fb112476812b666dc09cebb0123cfd4f538
Firman <moh.firmansyah@student.ub.ac.id>
2022-08-04 10:56:45 +0700

    Hosting

--- application/views/umkm.php
+++ application/views/umkm.php
@@ -73,7 +73,7 @@
     </section>
 
     <!-- UMKM Loop -->
-    <?php foreach ($list_data_umkm as $row) : ?>
+    <?php foreach ($data_umkm as $row) : ?>
     <section class="my-5 products-list d-flex justify-content-center align-items-center">
       <div class="card horizontal-card mb-3" style="max-width: 1100px;">
         <div class="row g-0">
