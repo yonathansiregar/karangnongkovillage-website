20ad33bc07b6df192f2f42ea6ae1fee92e4ba5dc
cnovandinata <95232744+cnovandinata@users.noreply.github.com>
2022-08-05 15:42:26 +0700

    a

--- application/views/modification.php
+++ application/views/modification.php
@@ -1,10 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <!-- META TAGS -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Icon -->
+  <link rel="icon" href="<?php echo base_url(); ?>assets/images/Logo.png" type="images/png">
+
+  <!-- CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/global.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/dashboard.css">
+
+  <!-- TITLE PAGE -->
+  <title><?php echo $title; ?></title>
 </head>
 <body>
     
