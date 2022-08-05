275f324be45afa519a908f65db23b2a944857d87
yonathansiregar <alfrenanday@gmail.com>
2022-08-05 10:36:27 +0700

    Change external css link for news detail page

--- application/views/news-detail.php
+++ application/views/news-detail.php
@@ -13,7 +13,7 @@
   <!-- CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/global.css">
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/news-page/berita.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/detailBerita.css">
 
   <!-- TITLE PAGE -->
   <title><?php echo $title; ?></title>
